cd84cd5624255a182999d70d4db485ddaa06e3f2
Lewis Wright <LewisW@users.noreply.github.com>
2023-07-15 20:34:49 +0100

    Improve caching example

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,18 +1,19 @@
 const {Puppeteer} = require("@carbonate/sdk/dist/browser");
 const SDK = require("@carbonate/sdk").default;
 const {describe, expect, test} = require("@jest/globals");
+const path = require("path");
 
 let browser = new Puppeteer(page);
 let sdk = new SDK(
     browser,
-    __dirname + '/cache',
+    path.join(__dirname, path.parse(__filename).name),
     // "<your user ID>", // TODO: Change me
     // "<your api key>" // TODO: Change me
 );
 
 setSDK(sdk);
 
-describe("Select Test", () => {
+describe("Example Test", () => {
   test("Select birthday from the event type dropdown", async () => {
     await sdk.load(
         'https://carbonate.dev/demo-form'
@@ -24,10 +25,8 @@ describe("Select Test", () => {
         await sdk.assertion('the event type dropdown should be set to Birthday')
     ).toBe(true);
   });
-});
 
-describe("Select Test Advanced", () => {
-  test("Select birthday from the event type dropdown", async () => {
+  test("Select birthday from the event type dropdown advanced", async () => {
     await sdk.load(
         'https://carbonate.dev/demo-form'
     );
